bc55c489957f41c8435dbaefccc97d881c9d3f58
Lidobix <pipoflutepouet@gmail.com>
2022-09-06 00:41:58 +0200

    intégraton d'un formulaire en remplacement des divs

---
+++ index.js
@@ -0,0 +1,9 @@
+window.document.addEventListener('DOMContentLoaded', function () {
+  const inputPeopleQty = this.getElementById('peopleQty');
+
+  inputPeopleQty.addEventListener('change', function () {
+    // const peopleQty = parseFloat(inputPeopleQty)
+    console.log(inputPeopleQty);
+    // if (){}
+  });
+});
