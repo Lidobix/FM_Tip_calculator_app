05cd8a03a887583426678e1138020a204235d0ce
Lidobix <pipoflutepouet@gmail.com>
2022-09-26 16:33:18 +0200

    gestion des actions du bouton reset

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 window.document.addEventListener('DOMContentLoaded', function () {
-  const inputPeopleQty = this.getElementById('peopleQty');
+  const peopleQty = this.getElementById('peopleQty');
+  const resetButton = this.getElementById('resetButton');
+  const billAmount = this.getElementById('billAmount');
+  let tipClicked = '';
 
-  inputPeopleQty.addEventListener('change', function () {
-    // const peopleQty = parseFloat(inputPeopleQty)
-    console.log(inputPeopleQty);
-    // if (){}
+  billAmount.addEventListener('input', () => {
+    resetButton.disabled = false;
   });
+
+  peopleQty.addEventListener('input', function () {
+    resetButton.disabled = false;
+  });
+
+  resetButton.addEventListener('click', () => {
+    peopleQty.value = 0;
+    billAmount.value = 0;
+    resetButton.disabled = true;
+  });
+
+  selectTips = function (tip) {
+    if (tipClicked != '') {
+      document.getElementById(tipClicked).classList.remove('tipClicked');
+      document.getElementById(tipClicked).classList.add('tipUnClicked');
+    } else {
+      document.getElementById('tip' + tip).classList.add('tipClicked');
+    }
+    tipClicked = 'tip' + tip;
+  };
 });
