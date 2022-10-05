daf4d5fc041e2013611601c1376b711e20922136
Lidobix <pipoflutepouet@gmail.com>
2022-10-05 12:56:28 +0200

    creation du message d'erreur + update des calculs

--- index.js
+++ index.js
@@ -3,27 +3,63 @@ window.document.addEventListener('DOMContentLoaded', function () {
   const resetButton = this.getElementById('resetButton');
   const billAmount = this.getElementById('billAmount');
   const customTip = this.getElementById('customTip');
+  const peopleLegend = this.getElementById('peopleLegend');
+  const tipAmount = this.getElementById('tipAmount');
+  const totalAmount = this.getElementById('totalAmount');
 
-  const tip = ['', 0];
+  const inputs = {
+    bill: 0,
+    tip: ['', 0],
+    people: 0,
+    error: false,
+    errorMessage: '',
+  };
 
+  (function createErrorMessage() {
+    const errorElement = document.createElement('div');
+    errorElement.innerText = "Can't be zero";
+    errorElement.classList.add('errorZeroPeople');
+    inputs.errorMessage = errorElement;
+    // console.log(inputs);
+  })();
+
+  // billAmount.addEventListener('input', () => {
+  //   resetButton.disabled = false;
+  // });
+
+  // peopleQty.addEventListener('input', function () {
+  //   resetButton.disabled = false;
+  // });
   billAmount.addEventListener('input', () => {
-    resetButton.disabled = false;
-  });
-
-  peopleQty.addEventListener('input', function () {
-    resetButton.disabled = false;
+    inputs.bill = billAmount.value;
+    console.log(inputs);
+    updateResult();
   });
 
   customTip.addEventListener('click', function () {
-    if (tip[0] != '') {
-      document.getElementById(tip[0]).classList.remove('tipClicked');
-      document.getElementById(tip[0]).classList.add('tipUnClicked');
+    if (inputs.tip[0] != '') {
+      document.getElementById(inputs.tip[0]).classList.remove('tipClicked');
+      document.getElementById(inputs.tip[0]).classList.add('tipUnClicked');
     }
     // customTip.classList.remove('inputUnClicked');
     // customTip.classList.add('inputClicked');
-    tip[0] = '';
-    tip[1] = '';
-    console.log(tip);
+    inputs.tip[0] = '';
+    inputs.tip[1] = '';
+    console.log(inputs);
+  });
+
+  peopleQty.addEventListener('input', () => {
+    // console.log(inputs);
+    if (peopleQty.value != 0 && inputs.error) {
+      deleteError();
+      inputs.error = false;
+    }
+    if (peopleQty.value == 0) {
+      generateError();
+      inputs.error = true;
+    }
+    inputs.people = peopleQty.value;
+    updateResult();
   });
 
   resetButton.addEventListener('click', () => {
@@ -33,15 +69,44 @@ window.document.addEventListener('DOMContentLoaded', function () {
   });
 
   selectTips = function (id) {
-    if (tip[0] === '') {
+    if (inputs.tip[0] === '') {
       document.getElementById('tip' + id).classList.add('tipClicked');
     } else {
-      document.getElementById(tip[0]).classList.remove('tipClicked');
-      document.getElementById(tip[0]).classList.add('tipUnClicked');
+      document.getElementById(inputs.tip[0]).classList.remove('tipClicked');
+      document.getElementById(inputs.tip[0]).classList.add('tipUnClicked');
       document.getElementById('tip' + id).classList.add('tipClicked');
     }
-    tip[0] = 'tip' + id;
-    tip[1] = id;
-    console.log(tip);
+    // inputs.tip[0] = 'tip' + id;
+    // inputs.tip[1] = id;
+    inputs.tip = ['tip' + id, id * 0.01];
+    if (inputs.people === 0) {
+      generateError();
+      inputs.error = true;
+    }
+    updateResult();
+    console.log(inputs);
   };
+
+  function generateError() {
+    peopleLegend.appendChild(inputs.errorMessage);
+  }
+
+  function deleteError() {
+    peopleLegend.removeChild(inputs.errorMessage);
+  }
+
+  function updateResult() {
+    if (inputs.tip != 0 && inputs.people != 0) {
+      tipAmount.innerText =
+        '$' +
+        Math.floor(((inputs.bill * inputs.tip[1]) / inputs.people) * 100) / 100;
+
+      totalAmount.innerText =
+        '$' +
+        Math.floor(
+          ((inputs.bill * (1 + inputs.tip[1])) / inputs.people) * 100
+        ) /
+          100;
+    }
+  }
 });
